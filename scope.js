da0e782db041117fc40e3245a713d42ee88a1324
hannahgreen1 <hannahgreen94@icloud.com>
2018-06-18 16:29:36 +0100

    episode 4 done

--- scope.js
+++ scope.js
@@ -1,12 +1,63 @@
-const scenario = {
-  murderer: 'Miss Scarlet',
-  room: 'Library',
-  weapon: 'Rope'
-};
+// episode 1
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Library',
+//   weapon: 'Rope'
+// };
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${scenario.murderer}.`;
+// }
+//
+// const verdict = declareMurderer();
+// console.log(verdict);
+// the murderer will be Miss Scarlet
 
-const declareMurderer = function() {
-  return `The murderer is ${scenario.murderer}.`;
+// episode 2
+// const murderer = 'Professor Plum';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+// error is trying to change the constant murderer name has already been declared
+
+// episode 3
+// let murderer = 'Professor Plum';
+//
+// const declareMurderer = function() {
+//   let murderer = 'Mrs. Peacock';
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+//
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
+// First Verdict:  The murderer is Mrs. Peacock.
+// Second Verdict:  The murderer is Professor Plum.
+
+
+// episode 4
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
+
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
 }
 
-const verdict = declareMurderer();
-console.log(verdict);
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
+// The suspects are Miss Scarlet, Professor Plum, Colonel Mustard.
+// Suspect three is Mrs. Peacock.
